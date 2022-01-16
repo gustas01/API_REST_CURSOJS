9212cc8c1dfea3d3dffad05094abde7ac37a8586
gustas01 <gustavohdlima@hotmail.com>
2022-01-16 13:20:12 -0300

    rotas de alunos

--- src/controllers/HomeController.js
+++ src/controllers/HomeController.js
@@ -1,16 +1,6 @@
-import Aluno from '../models/Aluno';
-
 class HomeController {
   async index(req, res) {
-    const novoAluno = await Aluno.create({
-      nome: 'Gustavo',
-      sobrenome: 'Lima',
-      email: 'gustavohdlima@hotmail.com',
-      idade: 27,
-      peso: 60,
-      altura: 1.7,
-    });
-    res.json(novoAluno);
+    res.json('Index');
   }
 }
 
